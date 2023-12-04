af0ea255522d33a27db37f782814900fa93a1d72
David Welsh <david.welsh@fglsports.com>
2023-12-04 12:22:14 -0700

    Initial commit

---
+++ src/test/java/com/example/wiremock/WiremockApplicationAnnotationTest.java
@@ -0,0 +1,70 @@
+package com.example.wiremock;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ContextConfiguration(classes = WiremockApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 5678)
+class WiremockApplicationAnnotationTest {
+
+    @Autowired
+    private RestClient.Builder builder;
+
+    @Test
+    void shouldPass() {
+        // GIVEN
+        WireMock.stubFor(get(
+                urlPathEqualTo("/test"))
+                .willReturn(aResponse()
+                        .withBody("{ \"success\": \"true\" }")
+                        .withStatus(200)
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                ));
+
+        var client = builder.baseUrl("http://localhost:5678").build();
+
+        // WHEN
+        var response = client.get().uri("/test").retrieve().toEntity(String.class);
+
+        // THEN
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals("{ \"success\": \"true\" }", response.getBody());
+    }
+
+    @Test
+    void shouldFail() {
+        // GIVEN
+        WireMock.stubFor(get(
+                urlPathEqualTo("/test"))
+                .willReturn(aResponse()
+                        .withBody("{ \"success\": \"true\" }")
+                        .withStatus(200)
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                ));
+
+        var client = builder.baseUrl("http://localhost:8080").build();
+
+        // WHEN
+        var exception = assertThrows(ResourceAccessException.class, () -> client.get().uri("/test").retrieve());
+
+        // THEN
+        assertNotNull(exception);
+    }
+
+}
